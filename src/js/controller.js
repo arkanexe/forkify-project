6cf10db2628bf7f7c60e73a7ab49ad7cfd55e4ec
Ayanbisi AbdulRahman <ayanyemiabdulrahman7@gmail.com>
2026-05-10 23:14:18 +0100

    Added a new welcome feature

--- src/js/controller.js
+++ src/js/controller.js
@@ -124,6 +124,10 @@ const controlAddRecipe = async function (newRecipe) {
   }
 };
 
+const newFeature = function () {
+  console.log('Welcome to the application');
+};
+
 /**
  * Registers all controller-to-view event subscriptions when the app starts.
  * @returns {void}
@@ -139,6 +143,8 @@ const init = function () {
   console.log('Welcome to Git AbdulRahman!!');
   searchView.addHandlerSearch(controlSearchResult);
   paginationView.addHandlerClick(controlPagination);
+
+  newFeature();
 };
 
 init();
